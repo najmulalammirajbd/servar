90ae4d4335e497bed35b20917857ae507a13dc27
najmulalammirajbd <najmulalammiraj@gmail.com>
2020-10-14 16:34:52 +0600

    my work done

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tfgke.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true });
+client.connect(err => {
+  const servicecollotion = client.db("creative-agency").collection("service");
+
+  app.post('/servicecollotion' , (req,res) => {
+      const service = req.body;
+      servicecollotion.insertOne(service)
+      .then(result => {
+          res.send(result.insertedCount > 0)
+      })
+  })
+});
+
+app.listen(process.env.PORT || port)
